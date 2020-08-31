69792deb1305e4759109871d4c6a830a5a64dd82
georgelyjo <47821682+georgelyjo@users.noreply.github.com>
2020-08-31 12:52:42 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import os
 operating = sys.platform
@@ -5,7 +6,15 @@ if(operating  in "win32" or operating in "win64"):
  os.system('pip3 install pyqrcode')
  os.system('pip3 install pypng')
 else:
+ os.system('chmod +x uploadme.py')
+ import os
+ myCmd = os.popen('pwd').read()
+ inputfile = "/uploadme.py"
+ cmd = myCmd.rstrip("\n")
+ cmd = cmd + inputfile
+ command = "sudo ln -s " + cmd + " /usr/bin/uploadme"
+ print(command)
+ os.system(command)
  os.system('pip3 install pyqrcode')
  os.system('pip3 install pypng')
  os.system('sudo apt-get install eog')
-
